8ea1c98c2ee1eaec1daab5dd24a008bed240b9ee
Rajnish Singh <rajnish.kumar2998@gmail.com>
2020-06-27 02:00:04 +0530

    Delete background_size.dart

--- lib/views/background_size.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-class BackgroundSize{
-  final String backgroundHeight;
- const BackgroundSize({this.backgroundHeight});
-  // var height = const backgroundHeight;
-}
