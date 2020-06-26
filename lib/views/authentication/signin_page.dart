3626f8c6bd688933ba6deec580c64cc40c8cb1cb
Rajnish Singh <rajnish.kumar2998@gmail.com>
2020-06-27 02:03:55 +0530

    Update and rename login_page.dart to signin_page.dart

--- lib/views/authentication/signin_page.dart
+++ lib/views/authentication/signin_page.dart
@@ -4,15 +4,6 @@ import '../../widgets/custom_button.dart';
 import '../../widgets/cubic_clipper.dart';
 import '../../utils/forms/sign_in_form.dart';
 import '../../widgets/logo_widget.dart';
-import '../background_size.dart';
-// TODO: for @ravi implemnt following
-// Add login with phone option
-// improve signUp option, like change place in App, or change UI of button etc.
-// Add Google plus signIn option at the bottom of the screen as decided in the layout or whrerver in page and anytype G+ signIn UI but it should match with the rest of UI and look appealing
-// add the wave style, Look and responsiveness of UI shouldn't change and the functionality too
-// Use color code const Color(0xFFF57C00) wherever needed
-// After then all depends on you that what best you can do with the UI
-// Bonus task if possible then add a "Welcome to Everly" type text on the top most of page only if it will look better because there is no where on login page everly name is mentioned
 
 class LoginPage extends StatelessWidget {
   // static const  double backgroundSize;
