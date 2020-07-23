9b423a217622c92e9713e0d889033afae513fdb9
Rajnish Singh <rajnish.kumar2998@gmail.com>
2020-07-24 01:09:31 +0530

    Update splash_Screen.dart

--- lib/views/splash_Screen.dart
+++ lib/views/splash_Screen.dart
@@ -21,12 +21,12 @@ class _SplashScreenState extends State<SplashScreen> {
 
   void initializingApplication() {
     Future.delayed(Duration(seconds: 2), () async {
-      // var user = await authHandler.getCurrentUser();
-      // if (user != null) {
-      //   Navigator.pushReplacementNamed(context, '/homePage');
-      // } else {
-      //   Navigator.pushReplacementNamed(context, '/logInPage');
-      // }
+       var user = await authHandler.getCurrentUser();
+       if (user != null) {
+         Navigator.pushReplacementNamed(context, '/homePage');
+       } else {
+         Navigator.pushReplacementNamed(context, '/logInPage');
+       }
       Navigator.pushReplacementNamed(context, '/homePage');
     });
   }
