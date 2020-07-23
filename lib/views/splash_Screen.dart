71e59ad467f9867a52e1b48712f017401081692b
Rajnish Singh <rajnish.kumar2998@gmail.com>
2020-07-24 01:10:31 +0530

    Update splash_Screen.dart

--- lib/views/splash_Screen.dart
+++ lib/views/splash_Screen.dart
@@ -27,7 +27,6 @@ class _SplashScreenState extends State<SplashScreen> {
        } else {
          Navigator.pushReplacementNamed(context, '/logInPage');
        }
-      Navigator.pushReplacementNamed(context, '/homePage');
     });
   }
 
